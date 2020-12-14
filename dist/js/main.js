e16400ed90439faea2821c6cc353a40256e0a184
woodtick88 <woodtick88@gmail.com>
2020-12-14 16:09:03 +0200

    убрать сортировку таблицы - 2

--- dist/js/main.js
+++ dist/js/main.js
@@ -107,31 +107,33 @@ $(function() {
 
 	// table sort on material page
 
-	const getSort = ({ target }) => {
-		const order = (target.dataset.order = -(target.dataset.order || -1));
-		const index = [...target.parentNode.cells].indexOf(target);
-		const collator = new Intl.Collator(['en', 'ru'], { numeric: true });
-		const comparator = (index, order) => (a, b) => order * collator.compare(
-			a.children[index].innerHTML,
-			b.children[index].innerHTML
-		);
+	// const getSort = ({ target }) => {
+	// 	const order = (target.dataset.order = -(target.dataset.order || -1));
+	// 	const index = [...target.parentNode.cells].indexOf(target);
+	// 	const collator = new Intl.Collator(['en', 'ru'], { numeric: true });
+	// 	const comparator = (index, order) => (a, b) => order * collator.compare(
+	// 		a.children[index].innerHTML,
+	// 		b.children[index].innerHTML
+	// 	);
 		
-		for(const tBody of target.closest('table').tBodies)
-			tBody.append(...[...tBody.rows].sort(comparator(index, order)));
+	// 	for(const tBody of target.closest('table').tBodies)
+	// 		tBody.append(...[...tBody.rows].sort(comparator(index, order)));
 
-		for(const cell of target.parentNode.cells)
-			cell.classList.toggle('sorted', cell === target);
-	};
+	// 	for(const cell of target.parentNode.cells)
+	// 		cell.classList.toggle('sorted', cell === target);
+	// };
 
-	if ( $('thead .table-sort').length ){
+	// if ( $('thead .table-sort').length ){
 		
-		document.querySelectorAll('thead .table-sort').forEach(tableTH => tableTH.addEventListener('click', () => getSort(event)));
+	// 	document.querySelectorAll('thead .table-sort').forEach(tableTH => tableTH.addEventListener('click', () => getSort(event)));
 
-	};
+	// };
 	
 	$('thead .table-sort').click(function() {
 		$(this).toggleClass('table-sort-active');
 	})
+
+
 
 
 	// search input
